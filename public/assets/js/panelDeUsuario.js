eb7e0de05ef8d1d9c8897e93794fbcc149f02353
sebastian <sebastianalexandroc@gmail.com>
2024-07-20 16:47:34 -0500

    creacion del panel de usuario con algunos adelantos a las funciones y adaptacion responsive

---
+++ public/assets/js/panelDeUsuario.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('userDropdown');
+    const menu = document.getElementById('dropdown-user');
+    const Bandeja = document.getElementById('Bandeja');
+    button.addEventListener('click', function () {
+      const isExpanded = button.getAttribute('aria-expanded') === 'true';
+      button.setAttribute('aria-expanded', !isExpanded);
+      menu.classList.toggle('hidden');
+    });
+  
+    document.addEventListener('click', function (event) {
+      if (!button.contains(event.target) && !menu.contains(event.target)) {
+        menu.classList.add('hidden');
+        button.setAttribute('aria-expanded', 'false');
+      }
+    });
+  });
+
+
+  Bandeja.addEventListener('click', function () {
+    const isExpanded = button.getAttribute('aria-expanded') === 'true';
+    button.setAttribute('aria-expanded', !isExpanded);
+    menu.classList.toggle('hidden');
+  });
+
+  document.addEventListener('click', function (event) {
+    if (!button.contains(event.target) && !menu.contains(event.target)) {
+      menu.classList.add('hidden');
+      button.setAttribute('aria-expanded', 'false');
+    }
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Selecciona todos los spans con un ID
+    const spans = document.querySelectorAll('span');
+
+    function checkWindowSize() {
+        if (window.innerWidth < 660) {
+            spans.forEach(span => span.classList.add('hidden'));
+        } else {
+            spans.forEach(span => span.classList.remove('hidden'));
+        }
+    }
+
+    // Check window size on load
+    checkWindowSize();
+
+    // Check window size on resize
+    window.addEventListener('resize', checkWindowSize);
+});
+
+
+  
