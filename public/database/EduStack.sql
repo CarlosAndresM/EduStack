16273ebdff6f18ba434e8477c489a02592d0cc80
Carlos Muñoz <carlosandresmunoza3@gmail.com>
2024-07-21 21:54:56 -0500

    Pequeñas modificaciones en el nombre de 2 archivos de panel de usuario y cambio de letra al archivo de creacion de base de datos sql de eduStack a EduStack

---
+++ public/database/EduStack.sql
@@ -0,0 +1,44 @@
+CREATE TABLE niveles_educativos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL
+);
+
+
+INSERT INTO niveles_educativos (nombre) VALUES
+('No especificado'),
+('Primaria'),
+('Secundaria'),
+('Técnico'),
+('Estudiante Universitario'),
+('Especialista');
+
+
+    CREATE TABLE usuarios (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(100) NOT NULL UNIQUE,
+        password VARCHAR(100) NOT NULL,
+        first_name VARCHAR(50) NOT NULL,
+        last_name VARCHAR(50) NOT NULL,
+        phone VARCHAR(20) NOT NULL,
+        userName VARCHAR(100) NOT NULL,
+        educational_level_id INT NOT NULL,
+        agreed_terms TINYINT(1) NOT NULL,
+        promotional_offers TINYINT(1) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (educational_level_id) REFERENCES niveles_educativos(id)
+    );
+
+CREATE TABLE usuarios_temporales (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    userName VARCHAR(100) NOT NULL,
+    educational_level_id INT NOT NULL,
+    agreed_terms TINYINT(1) NOT NULL,
+    promotional_offers TINYINT(1) NOT NULL,
+    verification_code VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
